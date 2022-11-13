17ae6d04aa9dc39d8b665d2225297937fe916c94
goddemi <goddemi0124@gmail.com>
2022-11-13 23:36:45 +0900

    ~15day 복습

--- 8day.js
+++ 8day.js
@@ -1,16 +1,18 @@
 // 3정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다.
 //  배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 return하도록 solution 함수를 완성해주세요.
 
-// function solution(numbers, direction) {
-//   return direction === "right"
-//     ? [numbers[numbers.length - 1], ...numbers.slice(0, numbers.length - 1)]
-//     : [...numbers.slice(1), numbers[0]];
-// }
+function solution(numbers, direction) {
+  return direction === "right"
+    ? [numbers[numbers.length - 1], ...numbers.slice(0, numbers.length - 1)]
+    : [...numbers.slice(1), numbers[0]];
+}
 
-// function solution(numbers, direction) {
-//     direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
-//     return numbers;
-// }
+function solution(numbers, direction) {
+  direction === "right"
+    ? numbers.unshift(numbers.pop())
+    : numbers.push(numbers.shift());
+  return numbers;
+}
 
 // shift() 메서드는 배열에서 첫 번째 요소를 제거하고, 제거된 요소를 반환합니다. 이 메서드는 배열의 길이를 변하게 합니다.
 //pop()도 마찬가지. 반환하고 배열의 길이 바뀜.
@@ -81,7 +83,7 @@
 //     return Array(num_list.length / n).fill([]).map(() => num_list.splice(0, n))
 // }
 
-// 여기서 배운 것은 splice를 하면 원본배열이 뜯겨져 나간다는 것이다. 그 원본배열을 더 사용하면 이미 사용했던 요소를 제외하고 연속으로 사용할 수 있는 것.
+여기서 배운 것은 splice를 하면 원본배열이 뜯겨져 나간다는 것이다. 그 원본배열을 더 사용하면 이미 사용했던 요소를 제외하고 연속으로 사용할 수 있는 것.
 
 // my_string은 "3 + 5"처럼 문자열로 된 수식입니다. 문자열 my_string이 매개변수로 주어질 때, 수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
 // const string = "1 + 3 + 5";
