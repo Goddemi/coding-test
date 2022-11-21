36ba6c6f5ff601c3abc39fb534db9538f4f42d72
goddemi <goddemi0124@gmail.com>
2022-11-21 23:44:12 +0900

    36day review

--- 21day.js
+++ 21day.js
@@ -1,33 +1,33 @@
-// 배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다
-//  이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
-//  단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다
+ 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
+ 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
 
-// arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
-// arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
-// 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
 
-// 제한사항
-// 배열 arr의 크기 : 1,000,000 이하의 자연수
-// 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
+제한사항
+배열 arr의 크기 : 1,000,000 이하의 자연수
+배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
 
-// 아래와 같이 풀었는데 효율성에서
+아래와 같이 풀었는데 효율성에서
 
-// const solution = (arr) => {
-//     let answer = []
-//      for (let i = 0; i<arr.length; i++) {
-//          answer = arr[i] !== arr[i+1] ? [...answer, arr[i]] : answer;
-//      }
-//         return answer
-//     }
+const solution = (arr) => {
+    let answer = []
+     for (let i = 0; i<arr.length; i++) {
+         answer = arr[i] !== arr[i+1] ? [...answer, arr[i]] : answer;
+     }
+        return answer
+    }
 
-//filter에 인덱스 값을 넣어서 풀 수 있었다. 이거 좋은 접근법 인듯.
+filter에 인덱스 값을 넣어서 풀 수 있었다. 이거 좋은 접근법 인듯.
 
-// const solution = (arr) => {
-//     return arr.filter((ele, i) => ele !== arr[i+1])
-// }
+const solution = (arr) => {
+    return arr.filter((ele, i) => ele !== arr[i+1])
+}
 
-// 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
-// const solution = (array, commands) =>
-//     commands.map((ele) => array.slice(ele[0]-1, ele[1]).sort((a,b) => a-b)[ele[2]-1])
+배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+const solution = (array, commands) =>
+    commands.map((ele) => array.slice(ele[0]-1, ele[1]).sort((a,b) => a-b)[ele[2]-1])
 
-// array destructuring을 사용하면 commands.map(([from,to,k])=>array.slice(from-1,to).sort((x,y)=>x>y)[k-1]) 이렇게 더 명시적으로 나타낼 수 있죠
+array destructuring을 사용하면 commands.map(([from,to,k])=>array.slice(from-1,to).sort((x,y)=>x>y)[k-1]) 이렇게 더 명시적으로 나타낼 수 있죠
--- 22day.js
+++ 22day.js
@@ -1,28 +1,28 @@
-// 별 다섯개..  참고: initialValue를 제공하지 않으면,
-// reduce()는 인덱스 1부터 시작해 콜백 함수를 실행하고 첫 번째 인덱스는 건너 뜁니다. initialValue를 제공하면 인덱스 0에서 시작합니다.
+별 다섯개..  참고: initialValue를 제공하지 않으면,
+reduce()는 인덱스 1부터 시작해 콜백 함수를 실행하고 첫 번째 인덱스는 건너 뜁니다. initialValue를 제공하면 인덱스 0에서 시작합니다.
 
 접근법은 비슷햇으나 나는 아래와 같이 작성했다. reduce의 acc값이 [0, 1]로 주어졌는데 왜 인덱스값에 접근이 안되는지 모르겠다. acc[0] <-- 이거 자체가 에러.
-// function solution(keyinput, board) {
-//     const moves = {
-//       up: [0, 1],
-//       down: [0, -1],
-//       left: [-1, 0],
-//       right: [1, 0],
-//     };
-//     const [rangeX, rangeY] = [Math.floor(board[0] / 2), Math.floor(board[1] / 2)];
+function solution(keyinput, board) {
+    const moves = {
+      up: [0, 1],
+      down: [0, -1],
+      left: [-1, 0],
+      right: [1, 0],
+    };
+    const [rangeX, rangeY] = [Math.floor(board[0] / 2), Math.floor(board[1] / 2)];
 
-//     return keyinput
-//       .map(key => moves[key])
-//       .reduce(
-//         ([x, y], [mX, mY]) => (Math.abs(x + mX) > rangeX || Math.abs(y + mY) > rangeY ? [x, y] : [x + mX, y + mY]),
-//         [0, 0]
-//       );
-//   }
+    return keyinput
+      .map(key => moves[key])
+      .reduce(
+        ([x, y], [mX, mY]) => (Math.abs(x + mX) > rangeX || Math.abs(y + mY) > rangeY ? [x, y] : [x + mX, y + mY]),
+        [0, 0]
+      );
+  }
 
-// const solution = (keyinput, board) => {
-//     const coordinate = {right : [1, 0], left : [-1,0], up : [0,1], down : [0,-1]};
-//         console.log(keyinput.map((ele) => coordinate[ele])
-//         .reduce((acc, cur) => {return [acc[0]+cur[0], acc[1] + cur[1]]}))
+const solution = (keyinput, board) => {
+    const coordinate = {right : [1, 0], left : [-1,0], up : [0,1], down : [0,-1]};
+        console.log(keyinput.map((ele) => coordinate[ele])
+        .reduce((acc, cur) => {return [acc[0]+cur[0], acc[1] + cur[1]]}))
 
-//     // <=parseInt(board/2)
-// }
+    // <=parseInt(board/2)
+}
