a19e30bdf77413dc2037c99d9ab92f46610ce048
goddemi <goddemi0124@gmail.com>
2022-12-13 12:51:32 +0900

    42day

--- 41day 다시 풀어보기.js
+++ 41day 다시 풀어보기.js
@@ -1,5 +1,11 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/140108
 
+// 문제를 여러번 읽어야 겠다.
+// 적어도 문제를 3번 읽는다.
+// x와 x가 아닌 다른 글자들이 나온 횟수를 각각 셉니다.
+//였는데 x와 x가 아닌 다른 글자들 중 나온 횟수가 x와 같은 어떤 하나의 글자.
+// 라고 인식했다.
+
 function solution(str) {
   const count = [1, 0];
   let sel = str[0];
