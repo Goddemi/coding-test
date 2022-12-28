c05252926f6507555759a8b4e7b9ae3f93f7373f
goddemi <goddemi0124@gmail.com>
2022-12-28 11:43:31 +0900

    42day

---
+++ 43day.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120956
+
+// 정규식활용에 비해 효율이 떨어짐. 잘푼듯 그래도. 재귀!
+
+function solution(babbling) {
+  const vocas = ["aya", "ye", "woo", "ma"];
+  let answer = 0;
+
+  const newArray = babbling.map((ele) => {
+    for (let i = 0; i <= vocas.length; i++) {
+      ele = ele.replace(vocas[i], "1");
+    }
+
+    return ele;
+  });
+
+  newArray.forEach((ele) => {
+    !isNaN(ele) && answer++;
+  });
+
+  return answer;
+}
